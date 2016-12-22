07f62ac0b967c1c1206f8628de597ebe73cd43c9
HarshaGUDev <harshagu.gubbi@gmail.com>
2016-12-22 18:22:48 +0530

    added style

--- resource/src/main/java/demo/PersonRepository.java
+++ resource/src/main/java/demo/PersonRepository.java
@@ -3,20 +3,8 @@ package demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
-@PreAuthorize("hasRole('ROLE_USER,ROLE_ADMIN')")
+
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends CrudRepository<Person, Long> {
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	  @Override
-	  Person save(Person s);
-	 
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	  @Override
-	  void delete(Long aLong);
-	  
-	  @PreAuthorize("hasRole('ROLE_ADMIN')")
-	  @Override
-	  Iterable<Person> findAll();
-	 
-	 
+
 	}
